eee1bd97230dd938848385f5f9222f0dcba4a146
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2025-10-19 19:53:13 +0700

    first commit

---
+++ src/task_c1/core.clj
@@ -0,0 +1,10 @@
+(ns task-c1.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn -main
+  []
+  (foo "From main"))
---
+++ test/task_c1/core_test.clj
@@ -0,0 +1,7 @@
+(ns task-c1.core-test
+  (:require [clojure.test :refer :all]
+            [task-c1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
