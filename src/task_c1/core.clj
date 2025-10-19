9de85f8ff600d972f3653a5f4839c0fead419777
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2025-10-19 21:52:09 +0700

    no-adjacent-repeats + test

--- src/task_c1/core.clj
+++ src/task_c1/core.clj
@@ -1,10 +1,28 @@
 (ns task-c1.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn no-adjacent-repeats
+  "Returns all strings of length N from alphabet with no equal subsequent characters."
+  [alphabet n]
+  (if (= n 1)
+    alphabet
+    (reduce
+      (fn [strings _]
+        (apply concat
+               (map
+                 (fn [s]
+                   (filter
+                     (fn [new-s]
+                       (not= (last s) (last new-s)))
+                     (map
+                       (fn [c] (str s c))
+                       alphabet)))
+                 strings)))
+      alphabet
+      (range (dec n)))))
 
 (defn -main
   []
-  (foo "From main"))
+  (println no-adjacent-repeats ["a" "b" "c"] 3)
+  (no-adjacent-repeats ["a" "b" "c"] 1)
+  (no-adjacent-repeats ["a" "b" "c"] 2)
+  )
--- test/task_c1/core_test.clj
+++ test/task_c1/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [task-c1.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest no-adjacent-repeats-test
+  (is (= (set (no-adjacent-repeats ["a" "b" "c"] 2))
+         #{"ab" "ac" "ba" "bc" "ca" "cb"})))
